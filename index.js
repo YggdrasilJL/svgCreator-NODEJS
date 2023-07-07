742bb864a40fc4d5a51ffc4fb9e2f6a24cceae0c
YggdrasilJL <yggdrasiljl@gmail.com>
2023-07-07 12:54:01 -0300

    add shape classes, rm jest, add color validation

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-const jest = require('jest')
 const inquirer = require('inquirer')
+const fs = require('fs')
 
 inquirer
 .prompt([
     {
         type: 'input',
-        message: 'Text of your SVG (Max 3 characters)',
+        message: 'Text of your SVG (Must be 1-3 characters.)',
         validate: input => {
-           if (input.length <= 3) {
+           if (input.length > 0 && input.length <= 3) {
             return true
            } else {
-            console.log('Oops!')
+            return Promise.reject('Invalid Input.')
            }
         },
         name: 'text'
@@ -20,8 +20,20 @@ inquirer
         message: 'What shape do you want your logo to be?',
         choices: ['Triangle', 'Circle', 'Square'],
         name: 'shape'
+    },
+    {
+        type: 'input',
+        message: 'What color do you want the shape to be? (Can be a color keyword, like "blue", or a hex.)',
+        name: 'color',
+        validate: input => {
+            if (input.length > 0) {
+             return true
+            } else {
+             return Promise.reject('Invalid Input.')
+            }
+         },
     }
 ])
-// .then(data => {
-
-// })
+.then(data => {
+    console.log(data)
+})
